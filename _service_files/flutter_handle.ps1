3fc789f125498bcd31f6fe6e25a42a14557d1727
mrfrolk <ekaterita20@gmail.com>
2025-03-28 08:26:41 +0500

    1

--- _service_files/flutter_handle.ps1
+++ _service_files/flutter_handle.ps1
@@ -7,9 +7,6 @@ flutter pub get
 dart run build_runner build
 dart run build_runner watch -d
 
-flutter pub run build_runner build --delete-conflicting-outputs
-flutter pub run build_runner watch --delete-conflicting-outputs
-
 flutter pub upgrade 
 flutter pub upgrade --major-versions
 
